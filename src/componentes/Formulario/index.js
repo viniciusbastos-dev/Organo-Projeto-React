55371f22841aa76bc40e3e8af1d006995ad73bb5
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-08-03 12:15:02 -0300

    Create the CampoTexto, Formulario and ListaSuspensa components

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,31 @@
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+
+const Formulario = () => {
+    const times = [
+        "Programação",
+        "Front-End",
+        "Data Science",
+        "Devops",
+        "UX e Design",
+        "Mobile",
+        "Inovação e Gestão",
+    ];
+    return (
+        <section className="formulario">
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Informe o endereço da imagem"
+                />
+                <ListaSuspensa label="Time" itens={times} />
+            </form>
+        </section>
+    );
+};
+
+export default Formulario;
